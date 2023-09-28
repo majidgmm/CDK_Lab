6f6958daa4efeab81af9a96fe583b007c38588b4
EC2 Default User <ec2-user@ip-172-31-17-242.us-east-2.compute.internal>
2023-09-28 00:32:36 +0000

    CDK Exam Lab

--- lib/cdkexam-stack.ts
+++ lib/cdkexam-stack.ts
@@ -1,16 +1,56 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import { Secret } from "aws-cdk-lib/aws-secretsmanager";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkexamStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    
+    //Create VPC
+    
+  const vpc = new ec2.Vpc(this, 'Vpc', {
+    ipAddresses: ec2.IpAddresses.cidr('10.30.0.0/16')
+  });    
 
-    // The code that defines your stack goes here
+    //Create EC2 in public Subnet
+  const instance1 = new ec2.Instance(this, 'Exam-Lab-EC2', {
+    vpc: vpc,
+    //securityGroup: securityGroup,
+    instanceName: 'public-EC2',
+    instanceType: ec2.InstanceType.of( 
+      ec2.InstanceClass.T2,
+      ec2.InstanceSize.MICRO
+    ),
+    machineImage: ec2.MachineImage.latestAmazonLinux({
+      generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+    }),
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkexamQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+  })  
+  
+  //Create SQL Queue
+  
+  new sqs.Queue(this, 'Queue');
+  
+  //Create SNS Topic
+  
+  const topic = new sns.Topic(this, 'MyTopic');
+  
+  //Create a Secret
+  
+  const masterUserSecret = new Secret(this, "metrodb-secrets", {
+    secretName: "metrodb-secrets",
+    description: "User credentials",
+    generateSecretString: {
+      secretStringTemplate: JSON.stringify({ username: "admin" }),
+      generateStringKey: "password",
+      passwordLength: 16,
+      excludePunctuation: true,
+    },
+  });  
+  
   }
 }
